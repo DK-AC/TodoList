b0a9342776574d8f8c7746a0c9189da7a384a0d3
DK-AC <denkacaj@gmail.com>
2022-02-09 22:18:21 +0300

    change name

--- src/store/reducers/todolists-reducer.ts
+++ src/store/reducers/todolists-reducer.ts
@@ -7,13 +7,13 @@ const initialState: Array<TodolistType> = []
 export const todolistsReducer = (state = initialState, action: GeneralType) => {
     switch (action.type) {
         case 'REMOVE-TODOLIST':
-            return [...state.filter(tl => tl.id != action.params.id)]
+            return [...state.filter(todo => todo.id != action.params.id)]
         case 'ADD-TODOLIST':
             return [...state, {id: v1(), title: action.params.title, filter: 'all'}]
         case 'CHANGE-TODOLIST-TITLE':
-            return [...state.map(m => m.id === action.params.id ? {...m, title: action.params.title} : m)]
+            return [...state.map(todo => todo.id === action.params.id ? {...todo, title: action.params.title} : todo)]
         case 'CHANGE-TODOLIST-FILTER':
-            return [...state.map(m => m.id === action.params.id ? {...m, filter: action.params.filter} : m)]
+            return [...state.map(todo => todo.id === action.params.id ? {...todo, filter: action.params.filter} : todo)]
         default:
             return state
     }
