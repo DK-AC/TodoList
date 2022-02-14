9e7a1ff6a9cc6a676f3c14fac74b7ea990b0aaa8
DK-AC <denkacaj@gmail.com>
2022-02-14 19:00:10 +0300

    create AppBarContainer

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,58 +1,12 @@
 import React from 'react';
 import './App.css';
-import {Todolist, TodolistType} from "../components/Todolist/Todolist";
-import {AddItemForm} from "../components/AddItemForm/AddItemForm";
-import AppBar from '@mui/material/AppBar';
-import Toolbar from '@mui/material/Toolbar';
-import IconButton from '@mui/material/IconButton';
-import Typography from '@mui/material/Typography';
-import Button from '@mui/material/Button';
-import Container from '@mui/material/Container';
-import {Menu} from '@mui/icons-material';
-import Grid from '@mui/material/Grid';
-import Paper from '@mui/material/Paper';
-import {addTodolistAC} from "../store/actions/todolistActions";
-import {useDispatch} from "react-redux";
-import {useAppSelector} from "../store/store";
+import {AppBarContainer} from "./AppBarContainer";
 
 
 function App() {
 
-    const dispatch = useDispatch()
-
-    const todolists = useAppSelector<TodolistType[]>(state => state.todolists)
-
-    const addTodolist = (title: string) => dispatch(addTodolistAC({title}))
-
     return (
-        <div>
-            <AppBar position="static">
-                <Toolbar>
-                    <IconButton edge="start" color="inherit" aria-label="menu">
-                        <Menu/>
-                    </IconButton>
-                    <Typography variant="h6">
-                        News
-                    </Typography>
-                    <Button color="inherit">Login</Button>
-                </Toolbar>
-            </AppBar>
-            <Container fixed>
-                <Grid container style={{padding: '20px'}}>
-                    <AddItemForm addItem={addTodolist}/>
-                </Grid>
-                <Grid container spacing={3}>
-                    {todolists.map(tl => {
-                        return (
-                            <Grid key={tl.id}>
-                                <Paper style={{padding: '10px'}}>
-                                    <Todolist todoId={tl.id}/>
-                                </Paper>
-                            </Grid>)
-                    })}
-                </Grid>
-            </Container>
-        </div>
+        <AppBarContainer/>
     );
 }
 
---
+++ src/app/AppBarContainer.tsx
@@ -0,0 +1,57 @@
+import React from 'react';
+import Toolbar from "@mui/material/Toolbar";
+import IconButton from "@mui/material/IconButton";
+import {Menu} from "@mui/icons-material";
+import Typography from "@mui/material/Typography";
+import Button from "@mui/material/Button";
+import './App.css';
+import AppBar from '@mui/material/AppBar';
+import Container from "@mui/material/Container";
+import Grid from "@mui/material/Grid";
+import {AddItemForm} from "../components/AddItemForm/AddItemForm";
+import Paper from "@mui/material/Paper";
+import {Todolist, TodolistType} from "../components/Todolist/Todolist";
+import {useDispatch} from "react-redux";
+import {useAppSelector} from "../store/store";
+import {addTodolistAC} from "../store/actions/todolistActions";
+
+export const AppBarContainer = () => {
+
+    const dispatch = useDispatch()
+
+    const todolists = useAppSelector<TodolistType[]>(state => state.todolists)
+
+    const addTodolist = (title: string) => dispatch(addTodolistAC({title}))
+
+    return (
+        <div>
+            <AppBar position="static">
+                <Toolbar>
+                    <IconButton edge="start" color="inherit" aria-label="menu">
+                        <Menu/>
+                    </IconButton>
+                    <Typography variant="h6">
+                        News
+                    </Typography>
+                    <Button color="inherit">Login</Button>
+                </Toolbar>
+            </AppBar>
+            <Container fixed>
+                <Grid container style={{padding: '20px'}}>
+                    <AddItemForm addItem={addTodolist}/>
+                </Grid>
+                <Grid container spacing={3}>
+                    {todolists.map(tl => {
+                        return (
+                            <Grid key={tl.id}>
+                                <Paper style={{padding: '10px'}}>
+                                    <Todolist todoId={tl.id}/>
+                                </Paper>
+                            </Grid>)
+                    })}
+                </Grid>
+            </Container>
+        </div>
+    );
+};
+
