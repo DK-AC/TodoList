472a010471b50def953283cab53b0fbc99def0cd
DK-AC <denkacaj@gmail.com>
2022-03-01 19:51:04 +0300

    clear stories task

--- src/stories/Task.stories.tsx
+++ src/stories/Task.stories.tsx
@@ -4,15 +4,8 @@ import {Task} from "../ui/Task/Task";
 
 export default {
     title: 'Todolist/Task',
-    component: Task,
-    argTypes: {},
 } as ComponentMeta<typeof Task>;
 
 
-const Template: ComponentStory<typeof Task> = (args) => <Task {...args} />;
+const Template: ComponentStory<typeof Task> = (args) => <Task {...args } />;
 
-export const TaskIsDoneStories = Template.bind({});
-TaskIsDoneStories.args = {
-    filteredTask: {id: '1', title: 'new', isDone: false},
-    todoId: 'todolistId:'
-};
