3c050efca040619e7931063110d25e85d48bbca8
DK-AC <denkacaj@gmail.com>
2022-03-21 12:13:36 +0300

    refactoring todolistReducer

--- src/bll/reducers/todolistReducer.ts
+++ src/bll/reducers/todolistReducer.ts
@@ -8,7 +8,6 @@ import {
 } from "../types/todolistTypes";
 import {TodolistType} from "../../dal/api/todolists-api";
 
-
 const initialState: TodolistType[] = []
 
 type initialStateType = typeof initialState
@@ -18,24 +17,13 @@ export const todolistReducer = (state = initialState, action: GeneralType): init
         case REMOVE_TODOLIST:
             return state.filter(todo => todo.id !== action.todolistId)
         case ADD_TODOLIST:
-            return [{
-                id: action.todolist.id,
-                title: action.todolist.title,
-                filter: 'all',
-                order: 0,
-                addedDate: ''
-            }, ...state]
+            return [action.todolist, ...state]
         case CHANGE_TODOLIST_TITLE:
-            return state.map(todo => todo.id === action.todolistId
-                ? {...todo, title: action.title}
-                : todo)
+            return state.map(todo => todo.id === action.todolistId ? {...todo, title: action.title} : todo)
         case CHANGE_TODOLIST_FILTER:
-            return state.map(todo => todo.id === action.todolistId
-                ? {...todo, filter: action.filter}
-                : todo)
+            return state.map(todo => todo.id === action.todolistId ? {...todo, filter: action.filter} : todo)
         case SET_TODOLISTS:
-            return action.todolists.map(todo => ({...todo, filter: 'all'}
-            ))
+            return action.todolists.map(todo => ({...todo, filter: 'all'}))
         default:
             return state
     }
