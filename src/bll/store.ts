ebbc741b137385de09ab7e0b940b35d77670fa2e
DK-AC <denkacaj@gmail.com>
2022-03-21 12:46:37 +0300

    add Redux devtools

--- src/bll/store.ts
+++ src/bll/store.ts
@@ -1,15 +1,22 @@
-import {applyMiddleware, combineReducers, createStore} from "redux";
+import {applyMiddleware, combineReducers, compose, createStore} from "redux";
 import {todolistReducer} from "./reducers/todolistReducer";
 import {TypedUseSelectorHook, useSelector} from "react-redux";
 import {tasksReducer} from "./reducers/tasksReducer";
 import thunk from "redux-thunk";
+
+declare global {
+    interface Window {
+        __REDUX_DEVTOOLS_EXTENSION_COMPOSE__?: typeof compose;
+    }
+}
+const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
 
 const appRootState = combineReducers({
     todolists: todolistReducer,
     tasks: tasksReducer
 })
 
-export const store = createStore(appRootState, applyMiddleware(thunk))
+export const store = createStore(appRootState, composeEnhancers(applyMiddleware(thunk)))
 
 export type AppRootStateType = ReturnType<typeof appRootState>
 export const useAppSelector: TypedUseSelectorHook<AppRootStateType> = useSelector
