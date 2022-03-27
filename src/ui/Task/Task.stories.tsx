e4a1f5f39ca011853f98b0857227e98f1b75b3be
DK-AC <denkacaj@gmail.com>
2022-03-27 12:20:45 +0300

    task stories

---
+++ src/ui/Task/Task.stories.tsx
@@ -0,0 +1,37 @@
+import React from 'react';
+import {ComponentMeta, ComponentStory} from "@storybook/react";
+import {Task} from "./Task";
+import {ReduxStoreProviderDecorator} from "../../stories/reduxStoreProviderDecorator";
+
+export default {
+    title: 'Todolist/Task',
+    component: Task,
+    decorators: [ReduxStoreProviderDecorator]
+} as ComponentMeta<typeof Task>;
+
+
+const Template: ComponentStory<typeof Task> = (args) => <Task {...args}/>
+
+export const TaskStories = Template.bind({});
+
+TaskStories.args = {
+    filteredTask: {
+        id: '1',
+        title: 'HTML',
+        status: 1,
+        todoListId: 'todoListId1',
+        addedDate: '',
+        deadline: '',
+        description: '',
+        order: 0,
+        priority: 1,
+        startDate: ''
+    },
+};
+
+
+
+
+
+
+
