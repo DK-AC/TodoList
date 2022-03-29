b9cffca080de250f9c2aa29f53c925057028a987
DK-AC <denkacaj@gmail.com>
2022-03-29 15:47:08 +0300

    refactoring taskReducer

--- src/bll/reducers/tasksReducer.ts
+++ src/bll/reducers/tasksReducer.ts
@@ -17,28 +17,23 @@ export const slice = createSlice({
             state[action.payload.task.todoListId].unshift(action.payload.task)
         },
         updateTaskAC(state, action: PayloadAction<{ todolistId: string, taskId: string, model: Partial<ModelTaskType> }>) {
-            return {
-                ...state, [action.payload.todolistId]: state[action.payload.todolistId]
-                    .map(task => task.id === action.payload.taskId ? {...task, ...action.payload.model} : task)
-            }
+            let task = state[action.payload.todolistId]
+            const index = task.findIndex(task => task.id === action.payload.taskId)
+            if (index !== -1) task[index] = {...task[index], ...action.payload.model}
         },
         getTasksAC(state, action: PayloadAction<{ todolistId: string, tasks: TaskType[] }>) {
-            return {...state, [action.payload.todolistId]: action.payload.tasks}
+            state[action.payload.todolistId] = action.payload.tasks
         },
     },
     extraReducers: {
         [addTodolistAC.type]: (state, action: PayloadAction<{ todolist: TodolistType }>) => {
-            return {...state, [action.payload.todolist.id]: []}
+            state[action.payload.todolist.id] = []
         },
         [removeTodolistAC.type]: (state, action: PayloadAction<{ todolistId: string }>) => {
-            const stateCopy = {...state};
-            delete stateCopy[action.payload.todolistId]
-            return stateCopy
+            delete state[action.payload.todolistId]
         },
         [setTodolistsAC.type]: (state, action: PayloadAction<{ todolists: TodolistType[] }>) => {
-            const stateCopy = {...state}
-            action.payload.todolists.forEach((tl: TodolistType) => stateCopy[tl.id] = [])
-            return stateCopy;
+            action.payload.todolists.forEach((tl: TodolistType) => state[tl.id] = [])
         },
     }
 })
@@ -47,31 +42,3 @@ export const tasksReducer = slice.reducer
 
 export const {getTasksAC, removeTaskAC, updateTaskAC, addTaskAC} = slice.actions
 
-// export const tasksReducer = (state: TasksStateType = initialTasksState, action: any): TasksStateType => {
-//     switch (action.type) {
-//         case REMOVE_TASK:
-//             return {...state, [action.todolistId]: state[action.todolistId].filter(task => task.id !== action.taskId)}
-//         case ADD_TASK:
-//             return {...state, [action.task.todoListId]: [action.task, ...state[action.task.todoListId]]}
-//         case UPDATE_TASK:
-//             return {
-//                 ...state, [action.todolistId]: state[action.todolistId]
-//                     .map(task => task.id === action.taskId ? {...task, ...action.model} : task)
-//             }
-//         case addTodolistAC.type:
-//             return {...state, [action.payload.todolist.id]: []}
-//         case removeTodolistAC.type:
-//             const stateCopy = {...state};
-//             delete stateCopy[action.payload.todolistId]
-//             return stateCopy
-//         case setTodolistsAC.type: {
-//             const stateCopy = {...state}
-//             action.payload.todolists.forEach((tl: TodolistType) => stateCopy[tl.id] = [])
-//             return stateCopy;
-//         }
-//         case GET_TASKS:
-//             return {...state, [action.todolistId]: action.tasks}
-//         default:
-//             return state
-//     }
-// }
