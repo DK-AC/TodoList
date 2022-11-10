348ff913f79a01c2635716fee99b47338ec87bc5
DK-AC <denkacaj@gmail.com>
2022-11-10 11:09:55 +0300

    Updates

---
+++ precache-manifest.269b06b32f4ed6f712f5f8e69f7eda99.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "caa0ae17063f86944359230579563d87",
+    "url": "/TodoList/index.html"
+  },
+  {
+    "revision": "d8617f5c808a880d3d4f",
+    "url": "/TodoList/static/css/main.ea39ab8e.chunk.css"
+  },
+  {
+    "revision": "7474bbfc571d90395817",
+    "url": "/TodoList/static/js/2.74670d10.chunk.js"
+  },
+  {
+    "revision": "ba69c2acccaf2fa71a7eef29df263a45",
+    "url": "/TodoList/static/js/2.74670d10.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d8617f5c808a880d3d4f",
+    "url": "/TodoList/static/js/main.40cfe74d.chunk.js"
+  },
+  {
+    "revision": "23d45432894410375589",
+    "url": "/TodoList/static/js/runtime-main.d0f80f2c.js"
+  }
+]);
