5a84bdcbdfed7447c3d28df08a1dab3c176e6ab2
DK-AC <denkacaj@gmail.com>
2022-11-10 14:58:15 +0300

    Updates

--- precache-manifest.3dce063f84870405f4a959b435218dbb.js
+++ precache-manifest.3dce063f84870405f4a959b435218dbb.js
@@ -1,23 +1,23 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "d62f976b10fd88d235603558d5d4ca71",
+    "revision": "dc7a0c629e17076ef7fba40bdbd6eb86",
     "url": "/TodoList/index.html"
   },
   {
-    "revision": "456beabf45c420347f00",
+    "revision": "40c63cca08bcab73da41",
     "url": "/TodoList/static/css/main.12106ce3.chunk.css"
   },
   {
-    "revision": "33e3079ed0c02ce21a20",
-    "url": "/TodoList/static/js/2.12a26d78.chunk.js"
+    "revision": "ebba167365f66bf2f1ab",
+    "url": "/TodoList/static/js/2.97bd05bc.chunk.js"
   },
   {
     "revision": "ba69c2acccaf2fa71a7eef29df263a45",
-    "url": "/TodoList/static/js/2.12a26d78.chunk.js.LICENSE.txt"
+    "url": "/TodoList/static/js/2.97bd05bc.chunk.js.LICENSE.txt"
   },
   {
-    "revision": "456beabf45c420347f00",
-    "url": "/TodoList/static/js/main.e94cf0d4.chunk.js"
+    "revision": "40c63cca08bcab73da41",
+    "url": "/TodoList/static/js/main.4c2b662d.chunk.js"
   },
   {
     "revision": "23d45432894410375589",
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/TodoList/precache-manifest.00276c6645c0fcccb50fbd040efcb044.js"
+  "/TodoList/precache-manifest.3dce063f84870405f4a959b435218dbb.js"
 );
 
 self.addEventListener('message', (event) => {
