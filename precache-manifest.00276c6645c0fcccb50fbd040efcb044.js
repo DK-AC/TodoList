e1c5d1fabf41093627c0ec386e20846d8c2febba
DK-AC <denkacaj@gmail.com>
2022-11-10 14:38:45 +0300

    Updates

---
+++ precache-manifest.00276c6645c0fcccb50fbd040efcb044.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d62f976b10fd88d235603558d5d4ca71",
+    "url": "/TodoList/index.html"
+  },
+  {
+    "revision": "456beabf45c420347f00",
+    "url": "/TodoList/static/css/main.12106ce3.chunk.css"
+  },
+  {
+    "revision": "33e3079ed0c02ce21a20",
+    "url": "/TodoList/static/js/2.12a26d78.chunk.js"
+  },
+  {
+    "revision": "ba69c2acccaf2fa71a7eef29df263a45",
+    "url": "/TodoList/static/js/2.12a26d78.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "456beabf45c420347f00",
+    "url": "/TodoList/static/js/main.e94cf0d4.chunk.js"
+  },
+  {
+    "revision": "23d45432894410375589",
+    "url": "/TodoList/static/js/runtime-main.d0f80f2c.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/TodoList/precache-manifest.798f443c988f273d51245b9b0ed0f4f0.js"
+  "/TodoList/precache-manifest.00276c6645c0fcccb50fbd040efcb044.js"
 );
 
 self.addEventListener('message', (event) => {
